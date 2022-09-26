995857dd813750d5c8f5f29833806bc748200562
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-26 12:11:59 +0500

    Compelete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const letters = string.split("");
+  let characterLenght = letters.length - 1;
+  let finalString = "";
+  for (let i = characterLenght; i >= 0; i--) {
+    finalString += letters[i];
+  }
+  return finalString;
 };
 
 // Do not edit below this line
