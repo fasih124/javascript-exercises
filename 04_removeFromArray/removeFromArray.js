0a3a53f47e8e22a560871b44e8ff326ac6c30afc
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-26 13:19:45 +0500

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...key) {
+  for (let i = 0; i < key.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === key[i]) {
+        arr.splice(j, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
