0a5216308bef0a7527c0f1ee86abd4b999348cce
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-26 11:55:29 +0500

    Complete RepeatString Evercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (var1, var2) {
+  let var3 = "";
+  if (var2 >= 0) {
+    for (let i = 0; i < var2; i++) {
+      var3 += var1;
+    }
+    return var3;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
