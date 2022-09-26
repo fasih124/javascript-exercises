a034ab64f043e3d8cffae1c14cf0750e26bbd18a
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-26 16:41:44 +0500

    done tempconversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  let reslut = (f - 32) * (5 / 9);
+  return Math.round(reslut * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let reslut = c * (9 / 5) + 32;
+  return Math.round(reslut * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
+//
