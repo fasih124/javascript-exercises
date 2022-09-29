b4ebbd671f4446916475dec0c43c7bc2a7fa1952
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-29 20:26:22 +0500

    Done calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  total = 0;
+  a.forEach((num) => {
+    total += num;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  product = 1;
+  a.forEach((num) => {
+    product *= num;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(4, 3);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  fact = 1;
+  for (let i = a; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
